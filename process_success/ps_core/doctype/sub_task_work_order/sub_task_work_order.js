e009e72f4e0002f80ab3cade3ed3556fa13de771
cmuell89 <cmuell89@gmail.com>
2017-03-21 21:28:18 -0700

    skeleton for web view of issues doctype

--- process_success/ps_core/doctype/sub_task_work_order/sub_task_work_order.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Process Success and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('sub_task_work_order', {
-	refresh: function(frm) {
-
-	}
-});
