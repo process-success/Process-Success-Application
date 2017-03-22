7f38e8057f0683a4743357b3034a8dfb286766a1
cmuell89 <cmuell89@gmail.com>
2017-03-21 21:32:07 -0700

    Mapping now references the geojson.data and geojson.coordinates for auto populating google maps object in .js file

--- process_success/templates/includes/mapping/map.js
+++ process_success/templates/includes/mapping/map.js
@@ -1,71 +1,33 @@
-function validateData(o) {
+(function(){
+    frappe.provide("ps.mapping")
 
-   var validateCoordinates = function (items) {
-        var validAll = items.every(function (item) {
-            var latLngs = item.getArray();
-            valid = latLngs.every(function (latLng) {
-                return isValidLatLng(latLng);
-            });
-            return valid;
-        });
-        return validAll;
-    };
+    var infowindow = new google.maps.InfoWindow();
 
-    var f = o.feature;
-    var geometry = f.getGeometry();
-    if (geometry.getType() == "MultiPolygon") {
-        var allCoords = geometry.getArray();
-        allCoords.forEach(function(coords) {
-            if (!validateCoordinates(coords.getArray())) {
-                document.getElementById('output').innerHTML += 'Geo JSON contains invalid lat/lng'; 
-            }
-        });
-    } else {
-        var coords = geometry.getArray();
-        if (!validateCoordinates(coords)) {
-             document.getElementById('output').innerHTML += 'Geo JSON contains invalid lat/lng'; 
-        }
-    }
-}
-
-
-function isValidLatLng(latLng) {
-    return latLng.lat() != 90;
-}
-
-frappe.ready(function(){
-    function initMap() {
-
-        var geojson = {
-              "type": "FeatureCollection",
-              "features": [
-                {
-                  "type": "Feature",
-                  "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                      -120.201,
-                      34.6122
-                    ]
-                  },
-                  "properties": {
-                    "marker-color": "#800b0b",
-                    "marker-size": "medium",
-                    "marker-symbol": "",
-                    "Name": "Margerum Wines"
-                  }
-                }
-              ]
-            };
+    function initMap(geojson, center) {
 
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: {lat: -28, lng: 137}
+            zoom: 10,
+            center: new google.maps.LatLng(center[1], center[0])
           });
 
-        console.log(vaidateData(geojson));
-        map.data.loadGeoJson(geojson);
+        map.data.addGeoJson(geojson);
+
+        google.maps.event.addListener(map, 'click', function() {
+             infowindow.close();
+        });
+
+        map.data.addListener('click', function(event) {
+            infowindow.setContent(event.feature.getProperty('name')+"<br>"+event.feature.getProperty('description'));
+            infowindow.setPosition(event.latLng);
+            infowindow.setOptions({pixelOffset: new google.maps.Size(0,-34)});
+            infowindow.open(map);
+          });
     }
 
-    google.maps.event.addDomListener(window, 'load', initMap);
-});
+    frappe.ready(function(){
+        var geojson = {{geojson}}
+
+        google.maps.event.addDomListener(window, 'load', initMap(geojson.data, geojson.center));
+    });
+
+})();
