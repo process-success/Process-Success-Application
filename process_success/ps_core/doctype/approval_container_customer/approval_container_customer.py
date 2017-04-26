e972dcd895eb9d78b988c55c1c8be9b5bcb40141
cmuell89 <cmuell89@gmail.com>
2017-04-25 17:53:19 -0700

    Core approval manager backend.
     On branch cmuell89/permissions
     Changes to be committed:
    	modified:   process_success/ps_core/doctype/approval_container_customer/approval_container_customer.json
    	modified:   process_success/ps_core/doctype/approval_container_customer/approval_container_customer.py
    	modified:   process_success/ps_core/doctype/approval_container_employee/approval_container_employee.json
    	modified:   process_success/ps_core/doctype/approval_container_employee/approval_container_employee.py
    	modified:   process_success/ps_core/doctype/approval_manager/approval_manager.json
    	modified:   process_success/ps_core/doctype/approval_manager/approval_manager.py
    	deleted:    process_success/ps_core/doctype/aproval_container/aproval_container.js
    	deleted:    process_success/ps_core/doctype/aproval_container/aproval_container.json
    	deleted:    process_success/ps_core/doctype/aproval_container/aproval_container.py
    	deleted:    process_success/ps_core/doctype/aproval_container/test_aproval_container.py
    	renamed:    process_success/ps_core/doctype/aproval_container/__init__.py -> process_success/utils/utilities.py

--- process_success/ps_core/doctype/approval_container_customer/approval_container_customer.py
+++ process_success/ps_core/doctype/approval_container_customer/approval_container_customer.py
@@ -6,5 +6,37 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class approval_container_customer(Document):
-	pass
+    def validate(self):
+        pass
+
+    def update_container(self):
+        customer = frappe.get_doc("Customer", self.customer)
+        user = frappe.get_doc("User", customer.user)
+        if self.status == "Approved":
+            if not self.duplicate_vineyard_container_check(customer):
+                vineyard_container = frappe.get_doc({
+                    "doctype": "vineyard_container",
+                    "parent": self.customer,
+                    "parenttype": "Customer"
+
+                })
+                vineyard_container.vineyard = self.vineyard
+                customer.append("vineyards", vineyard_container)
+            customer.status = self.status
+            user.set("roles", [{"role": "Customer"}])
+            user.save()
+            customer.save()
+        elif self.status == "Disabled":
+            customer.status = self.status
+            user.enabled = False
+            user.save()
+            customer.save()
+
+    def duplicate_vineyard_container_check(self, customer):
+        vineyard_containers = customer.get_all_children()
+        for container in vineyard_containers:
+            if container.vineyard == self.vineyard:
+                return True
+        return False
--- process_success/ps_core/doctype/approval_container_employee/approval_container_employee.py
+++ process_success/ps_core/doctype/approval_container_employee/approval_container_employee.py
@@ -6,5 +6,29 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+
 class approval_container_employee(Document):
-	pass
+
+    def validate(self):
+        pass
+
+    def update_container(self):
+        employee = frappe.get_doc("Employee", self.employee)
+        user = frappe.get_doc("User", employee.user)
+        if self.status == "Approved":
+            employee.compensation_type = self.compensation_type
+            employee.compensation = self.compensation
+            employee.status = self.status
+            if self.role == "Employee":
+                print("just employee")
+                user.set("roles", [{"role": "Employee"}])
+                user.save()
+            else:
+                user.set("roles", [{"role": "Employee"}, {"role": self.role}])
+                user.save()
+            employee.save()
+        elif self.status == "Disabled":
+            employee.status = self.status
+            user.enabled = False
+            user.save()
+            employee.save()
