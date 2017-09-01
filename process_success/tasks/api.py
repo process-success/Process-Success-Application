7d9b806a90e61d04157993586da521b54999ff4f
derek brenner <derekbrenner@gmail.com>
2017-09-01 12:07:51 -0700

    adding fixtures for emailing

--- process_success/tasks/api.py
+++ process_success/tasks/api.py
@@ -6,3 +6,40 @@ from frappe.utils import cint, nowdate, nowtime, cstr, add_days, flt, today
 from frappe.sessions import get_csrf_token
 import json
 import unicodedata
+from process_success.ps_core.api import create_doc, update_doc
+
+#Tasks have
+# Task: docType -describes what it is
+# TaskData: multi doctype
+#   - variable data/properties for the task
+#   - varies fields dependent type of task
+# TaskTypeOptions: doctype
+#   - contains the global const data assotiated specific tasks like variable pricing
+
+###
+# CREATE TASK TYPE
+###
+
+# create TaskData doctype Doctype (document type of Doctype)
+# create a taskTypeOptions
+
+
+###
+# UPDATE TASK TYPE
+###
+
+###
+# Get Tasks
+###
+
+###
+# Create task
+##
+
+###
+# Update Task
+###
+
+###
+# Remove Task
+###
