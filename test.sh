59e276ca561a95a185a5582d04bcbc4e7b1f1f04
derek brenner <derekbrenner@gmail.com>
2017-09-08 11:20:04 -0700

    changes

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 git add -A
 git commit -am "changes"
 git push
-git chekout stage
+git checkout stage
 git merge develop
 git push
 git checkout master
 git merge stage
 git push
-git checout develop
+git checkout develop
 
