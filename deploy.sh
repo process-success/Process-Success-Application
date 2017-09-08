c9150dcfc681e6d75fcb3d1c644641d1ffc443ae
derek brenner <derekbrenner@gmail.com>
2017-09-08 11:21:32 -0700

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,11 @@
-# read -p "Are you sure gulp is off? " -n 1 -r
-# echo    # (optional) move to a new line
-# if [[ $REPLY =~ ^[Yy]$ ]]
-# then
-#
-# git add -A
-# git commit -am $1
-# git push
-#
-#
-# git checkout stage
-# git merge develop
-# git commit -am $1
-# git push
-# git checkout develop
-#
-# fi
+git add -Au
+git commit -am "changes"
+git push
+git checkout stage
+git merge develop
+git push
+git checkout master
+git merge stage
+git push
+git checkout develop
 
-# loop over the 
